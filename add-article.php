c9c85f7a214f021801dafea349ae9563ca989894
Malik59 <mal.k@laposte.net>
2024-01-29 15:49:26 +0100

    nettoyage formulaire

--- add-article.php
+++ add-article.php
@@ -1,3 +1,65 @@
+<?php
+const ERROR_REQUIRED = "Veuillez renseigner ce champ";
+const ERROR_TITLE_TOO_SHORT = "Le titre est trop court";
+const ERROR_CONTENT_TOO_SHORT = "L'article est trop court";
+const ERROR_IMAGE_URL = "L'image doit être une url valide";
+$filename = __DIR__ . "/data/articles.json";
+$errors = [
+    "title" => "",
+    "image" => "",
+    "category" => "",
+    "content" => ""
+];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $_POST = filter_input_array(INPUT_POST, [
+        "title" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        "image" => FILTER_SANITIZE_URL,
+        "category" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        "content" => [
+            "filter" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            "flages" => FILTER_FLAG_NO_ENCODE_QUOTES
+        ]
+    ]);
+
+    $title = $_POST["title"] ?? "";
+    $image = $_POST["image"] ?? "";
+    $category = $_POST["category"] ?? "";
+    $content = $_POST["content"] ?? "";
+
+    if (!$title) {
+        $errors["title"] = ERROR_REQUIRED;
+    } else if (mb_strlen($title) < 5) {
+        $errors = ERROR_TITLE_TOO_SHORT;
+    }
+
+    if (!$image) {
+        $errors["image"] = ERROR_REQUIRED;
+    } else if (!filter_var($image, FILTER_VALIDATE_URL)) {
+        $errors = ERROR_IMAGE_URL;
+    }
+
+    if (!$category) {
+        $errors["category"] = ERROR_REQUIRED;
+    }
+    
+    if (!$content) {
+        $errors["content"] = ERROR_REQUIRED;
+    } else if (mb_strlen($content) < 50) {
+        $errors = ERROR_CONTENT_TOO_SHORT;
+    }
+
+    if (empty(array_filter($errors,fn ($e) => $e !== ""))) {
+        echo "c'est ok";
+    } else {
+        print_r($errors);
+    }
+
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
