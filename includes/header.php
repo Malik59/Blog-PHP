7a5ab626281d6caec9b22b298840043c00bc34a1
Malik59 <mal.k@laposte.net>
2024-01-29 11:51:12 +0100

    création des différents dossiers

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <div class="logo">My Blog</div>
+</header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require_once __DIR__ . "/includes/head.php" ?>
+    <title>Blog Version 1</title>
+</head>
+<body>
+    <div class="container">
+    <?php require_once __DIR__ . "/includes/header.php" ?>
+        
+    <div class="content">
+        
+    </div>
+
+    <?php require_once __DIR__ . "/includes/footer.php" ?>
+    
+        
+    </div>
+
+</body>
+</html>
