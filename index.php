350f6ac73e9bb3a220d9e9f268d29ddd301126c8
Malik59 <mal.k@laposte.net>
2024-01-30 11:40:25 +0100

    fonctions tableaux => regroupement articles par catégorie

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 $filename = __DIR__ . "/data/articles.json";
 $articles = [];
+$categories= [];
 
 if (file_exists($filename)) {
     $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $cattmp = array_map(fn ($a) => $a["category"] ,$articles);
+    $categories = array_reduce($cattmp, function ($acc, $cat) {
+        if (isset($acc[$cat])) {
+            $acc[$cat] ++;
+        } else {
+            $acc[$cat] = 1;
+        }
+        return $acc;
+    },[]);
+    $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+        if (isset($acc[$article['category']])) {
+          $acc[$article['category']] = [...$acc[$article['category']], $article];
+        } else {
+          $acc[$article['category']] = [$article];
+        }
+        return $acc;
+      }, []);
+
+      echo "<pre>";
+      print_r($articlePerCategories);
+      echo "</pre>";
 }
 
 ?>
