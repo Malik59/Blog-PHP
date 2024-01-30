96eb091b966f77db1c56e32e6d8a99f1a646a2b9
Malik59 <mal.k@laposte.net>
2024-01-30 16:10:33 +0100

    articles affichés par catégorie

--- index.php
+++ index.php
@@ -1,37 +1,34 @@
 <?php
 $filename = __DIR__ . "/data/articles.json";
 $articles = [];
-$categories= [];
+$categories = [];
 
 if (file_exists($filename)) {
     $articles = json_decode(file_get_contents($filename), true) ?? [];
-    $cattmp = array_map(fn ($a) => $a["category"] ,$articles);
+    $cattmp = array_map(fn ($a) => $a["category"], $articles);
     $categories = array_reduce($cattmp, function ($acc, $cat) {
         if (isset($acc[$cat])) {
-            $acc[$cat] ++;
+            $acc[$cat]++;
         } else {
             $acc[$cat] = 1;
         }
         return $acc;
-    },[]);
+    }, []);
     $articlePerCategories = array_reduce($articles, function ($acc, $article) {
         if (isset($acc[$article['category']])) {
-          $acc[$article['category']] = [...$acc[$article['category']], $article];
+            $acc[$article['category']] = [...$acc[$article['category']], $article];
         } else {
-          $acc[$article['category']] = [$article];
+            $acc[$article['category']] = [$article];
         }
         return $acc;
-      }, []);
-
-      echo "<pre>";
-      print_r($articlePerCategories);
-      echo "</pre>";
+    }, []);
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <?php require_once __DIR__ . "/includes/head.php" ?>
     <link rel="stylesheet" href="public/css/index.css">
@@ -40,25 +37,34 @@ if (file_exists($filename)) {
 
 <body>
     <div class="container">
-    <?php require_once __DIR__ . "/includes/header.php" ?>
-        
-    <div class="content">
-        <div class="articles-container">
-            <?php foreach ($articles as $a): ?>
-                <div class="article block">
-                    <div class="overflow">
-                        <div class="img-container" style="background-image:url(<?= $a["image"] ?>);"></div>
-                    </div>
-                    <h2><?= $a["title"] ?></h2>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
+        <?php require_once __DIR__ . "/includes/header.php" ?>
 
-    <?php require_once __DIR__ . "/includes/footer.php" ?>
-    
-        
+        <div class="content">
+
+            <div class="category-container">
+                <?php foreach ($categories as $cat => $num) : ?>
+                    <h2><?= $cat ?></h2>
+                    <div class="articles-container">
+                        <?php foreach ($articlePerCategories[$cat] as $a) : ?>
+                            <div class="article block">
+                                <div class="overflow">
+                                    <div class="img-container" style="background-image:url(<?= $a["image"] ?>);"></div>
+                                </div>
+                                <h3><?= $a["title"] ?></h3>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
+
+        </div>
+
+        <?php require_once __DIR__ . "/includes/footer.php" ?>
+
+
     </div>
 
 </body>
+
 </html>
