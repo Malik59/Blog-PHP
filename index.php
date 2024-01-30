55e3e48b8c880807ab354723cd071a2037b2f744
Malik59 <mal.k@laposte.net>
2024-01-30 09:33:07 +0100

    affichage image+titre des articles sur la page d'accueil

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+$filename = __DIR__ . "/data/articles.json";
+$articles = [];
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,7 +21,16 @@
     <?php require_once __DIR__ . "/includes/header.php" ?>
         
     <div class="content">
-        
+        <div class="articles-container">
+            <?php foreach ($articles as $a): ?>
+                <div class="article block">
+                    <div class="overflow">
+                        <div class="img-container" style="background-image:url(<?= $a["image"] ?>);"></div>
+                    </div>
+                    <h2><?= $a["title"] ?></h2>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 
     <?php require_once __DIR__ . "/includes/footer.php" ?>
