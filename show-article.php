c7bd13d267304e9b68772689bd31bdaf49620e47
Malik59 <mal.k@laposte.net>
2024-01-31 13:43:59 +0100

    édition d'un article

--- show-article.php
+++ show-article.php
@@ -20,27 +20,29 @@ if (!$id) {
 <!DOCTYPE html>
 <html lang="fr">
 
-  <head>
-      <?php require_once 'includes/head.php' ?>
-      <link rel="stylesheet" href="/public/css/show-article.css">
-      <title>Article</title>
-  </head>
+<head>
+  <?php require_once 'includes/head.php' ?>
+  <link rel="stylesheet" href="/public/css/show-article.css">
+  <title>Article</title>
+</head>
 
-  <body>
-    <div class="container">
-      <?php require_once 'includes/header.php' ?>
-      <div class="content">
-        <div class="article-container">
-          <a class="article-back" href="/">Retour à la liste des articles</a>
-          <div class="article-cover-img" style="background-image:url(<?= $article['image'] ?>)"></div>
-          <h1 class="article-title"><?= $article['title'] ?></h1>
-          <div class="separator"></div>
-          <p class="article-content"><?= $article['content'] ?></p>
+<body>
+  <div class="container">
+    <?php require_once 'includes/header.php' ?>
+    <div class="content">
+      <div class="article-container">
+        <a class="article-back" href="/">Retour à la liste des articles</a>
+        <div class="article-cover-img" style="background-image:url(<?= $article['image'] ?>)"></div>
+        <h1 class="article-title"><?= $article['title'] ?></h1>
+        <div class="separator"></div>
+        <p class="article-content"><?= $article['content'] ?></p>
+        <div class="form-actions">
+          <a class="btn btn-edit" href="/form-article.php?id=<?= $article['id'] ?>">Editer l'article</a>
         </div>
       </div>
-      <?php require_once 'includes/footer.php' ?>
     </div>
-
-  </body>
+    <?php require_once 'includes/footer.php' ?>
+  </div>
+</body>
 
 </html>
