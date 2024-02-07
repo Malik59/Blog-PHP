4cac6dc0342afb810996b74ec6eb1eee202ff194
Malik59 <mal.k@laposte.net>
2024-02-07 15:23:13 +0100

    initialisation de la base de données - récupération des articles sur l'application depuis le fichier JSON effectuée.

--- database.php
+++ database.php
@@ -1,8 +1,14 @@
 <?php
 
 
+// $user = getenv('DB_USER'); 
+// $pwd = getenv('DB_PWD'); --> pour sécuriser: one ne voit pas le nom utilisateur et le mot de passe: il faut les écrire directement dans le terminal lorsque l'on lance le serveur
+$user = "root";
+$pwd = "M@lik5959";
+
+
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=blog", "root", "M@lik5959", [
+    $pdo = new PDO("mysql:host=localhost;dbname=blog", $user, $pwd, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 $pdo = require_once "./database.php";
-$filename = __DIR__ . "/data/articles.json";
-$articles = [];
+$statement = $pdo->prepare("SELECT * FROM article");
+$statement->execute();
+$articles = $statement->fetchAll();
 $categories = [];
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $selectedCat = $_GET["cat"] ?? "";
 
-if (file_exists($filename)) {
-    $articles = json_decode(file_get_contents($filename), true) ?? [];
+if (count($articles)) {
     $cattmp = array_map(fn ($a) => $a["category"], $articles);
     $categories = array_reduce($cattmp, function ($acc, $cat) {
         if (isset($acc[$cat])) {
